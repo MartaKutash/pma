18dea7ebd7c594d628743adab940bfc5cb690c4c
Marta <nameless_mk@tut.by>
2023-04-14 05:20:57 +0300

    feat: add boards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './board/board.component';
+import { DeleteAccountComponent } from './delete-account/delete-account.component';
 import { ListingpageComponent } from './listingpage/listingpage.component';
 import { LoginComponent } from './login/login.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
@@ -11,7 +13,9 @@ const routes: Routes = [
   {path: 'main', component: MainpageComponent},
   {path: 'login', component: LoginComponent},
   {path: 'listing', component: ListingpageComponent},
-  {path: 'sign', component: SignComponent}
+  {path: 'sign', component: SignComponent},
+  {path: 'delete', component: DeleteAccountComponent},
+  {path: 'board', component: BoardComponent},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MediaObserver, MediaChange } from '@angular/flex-layout/core';
+import { Subscription } from 'rxjs';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 
 @Component({
@@ -6,8 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pma';
+  public showContainer: boolean | undefined;
+  constructor(public breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(['(min-width: 320px)'])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.showContainer = true;
+        } else {
+          this.showContainer = false;
+        }
+      });
+  }
 }
 
 
